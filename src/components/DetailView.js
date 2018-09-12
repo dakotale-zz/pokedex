6f2ca58d5e5e5fe1745577d71e9f02cc1be724c0
Dakota Lester <dakotale@buffalo.edu>
2018-09-11 21:37:40 -0400

    Change from the provide
    Include more info and customize stuff

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './styles/DetailView.css';
 
 const DetailView = ({ pokemon }) => {
-  const { id, name, sprite, type } = pokemon;
+  const { id, name, sprite, type, weight } = pokemon;
 
   return (
     <section className="detail-view">
       <img src={sprite} className='sprite-image' alt="sprite"/>
       <div className='data-wrapper'>
-        <h1 className='data-name'>ID: {id} {name}</h1>
+        <h1 className='data-id'>ID: {id}</h1>
+        <p className="data-name">Name: {name}</p>
         <p className="data-char">Type: {type}</p>
+        <p className='data-weight'>Weight: {weight} kg</p>
       </div>
     </section>
   )
